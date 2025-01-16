9e0124b010a4dff1060256a932653d4dad71db8a
Lennard <168357+lennardv2@users.noreply.github.com>
2025-01-16 23:41:49 +0100

    Add support for opening remote workspaces and create new remote workspace functionality

--- src/views/list.ts
+++ src/views/list.ts
@@ -210,6 +210,11 @@ export function getListWebview(groups: { name: string; workspaces: (WorkspaceRef
             <button class="add-button" onclick="addWorkspaceToGroup('${group.name}')" title="Add Workspace to Group">
                 <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg> 
             </button>
+            <button class="add-button" onclick="addRemoteWorkspaceToGroup('${group.name}')" title="Add Remote Workspace to Group">
+              <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <!-- ...icon data... -->
+              </svg> 
+            </button>
           </div>
           <ul>
             ${group.workspaces.map(workspace => `
@@ -275,6 +280,10 @@ export function getListWebview(groups: { name: string; workspaces: (WorkspaceRef
 
         function renameGroup(groupName) {
           vscode.postMessage({ command: 'renameGroup', groupName });
+        }
+
+        function addRemoteWorkspaceToGroup(groupName) {
+          vscode.postMessage({ command: 'createNewRemoteWorkspace', groupName });
         }
 
         function onDragStart(event, directory) {
